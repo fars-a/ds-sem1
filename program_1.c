461bec5f558b354f1105a4fbd0b17cc779fe2a7f
fars <168010114+fars-a@users.noreply.github.com>
2025-11-15 09:40:48 +0530

    Add files via upload

---
+++ program_1.c
@@ -0,0 +1,25 @@
+/*
+1.program to demonstrate the use of storage classes (local variable, global variable, static variable, register variable) in C.
+@Farsan_Suneer
+Date_of_completion: 13/07/2025
+*/
+#include <stdio.h>
+int g=10;//global variable
+void scope()
+{
+    int l=1;//local variable
+    register int r=2;//register variable
+    static int s=0;//static variable
+    printf("\nLocal variable=%d",l);
+    printf("\nRegister variable=%d",r++);
+    printf("\nStatic variable=%d",s++);
+    printf("\nglobal variable=%d\n",g);
+}
+int main()
+{
+    printf("\nFirst call to the function");
+    scope();
+    printf("\nSecond call to the function");
+    scope();
+    return 0;
+}
